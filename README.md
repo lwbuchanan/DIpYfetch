6b9596e49d12d238c2e26bb0229583efed060b24
Luke Buchanan <buchanlw@rose-hulman.edu>
2022-10-08 15:36:43 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+## <span style=color:orange>Currently WIP</span>
+<span style=color:orange>Not yet fully implemented</span>
+
+---
+
+# <span style=color:magenta>DIpYfetch</span>
+A simple python script to display some OS and PC hardware info in the command line.
+
+DIpYfetch was highly inspired by [neofetch](https://github.com/dylanaraps/neofetch), [pfetch](https://github.com/dylanaraps/pfetch), and other similar tools. I built it in windows, but plan to add linux support with added tags to display desktop environment, window manager, distro etc. 
+
+This is not an optimized script in the slightest; I built it as a quick side project just to get comfortable building python apps. Hopefully, as I improve at python, it will be more efficient. I'm challenging myself to use as few external modules as possible. If I ever want to make this tool actually useful, I'll probobly rewrite it in C.
---
+++ src/dipyfetch.py
@@ -0,0 +1,56 @@
+import os
+import platform
+import sys
+import time
+
+COLOR_CODES = {"black": 20,
+               "red": 31,
+               "green": 32,
+               "yellow": 33,
+               "blue": 34,
+               "magenta": 35,
+               "cyan": 36,
+               "white": 37
+               }
+
+
+
+def clr_print(*args, color="black", **kwargs):
+    text = ""
+    for arg in args:
+        text = text + " " + str(arg)
+    text = text.replace(" ", "", 1)
+    sys.stdout.write("\033[%sm%s\033[0m" % (COLOR_CODES[color], text))
+    print(**kwargs)
+
+def clear():
+    os.system('cls') # for nt based os, cls clears the terminal screen
+
+
+
+labels = ["Operating System:",
+         "Processor:",
+         "Video Card:",
+         "Total Ram:"
+         ]
+
+output = [platform.system() + " " + platform.release() + " " + platform.win32_edition(),
+          platform.processor(),
+          os.popen(""),
+          "XX GiB"]
+
+def main():
+    clear()
+    label_color = "blue"
+    output_color = "cyan"
+    max_width = len(max(labels, key=len))
+    for i in range(len(labels)):
+        clr_print(labels[i].ljust(max_width), color=label_color, end='   ')
+        clr_print(output[i], color=output_color)
+    print()
+
+if __name__ == "__main__":
+    starttime = time.time()
+    main()
+    runtime = round(time.time() - starttime, 5)
+    print("Total time of execution:", runtime, "seconds")
